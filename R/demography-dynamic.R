157e9494ae870493f99c026f94d47f42dfb42f35
slwu89 <10673535+slwu89@users.noreply.github.com>
2022-11-30 11:29:07 -0800

    parameter setters for demographic models

--- R/demography-dynamic.R
+++ R/demography-dynamic.R
@@ -36,3 +36,24 @@ make_index_H.dynamic <- function(pars) {
   pars$H_ix <- seq(from = pars$max_ix+1, length.out = pars$nStrata)
   pars$max_ix <- tail(pars$H_ix, 1)
 }
+
+#' @title Make parameters for forced (trace) human demography model
+#' @param pars an [environment]
+#' @param b a list containing vectors of birth rates for each state in \eqn{\mathcal{X}}; each vector should be of length `nStrata`
+#' @param d a list containing vectors of death rates for each state in \eqn{\mathcal{X}}; each vector should be of length `nStrata`
+#' @param m a list containing vectors of ageing rates for each state in \eqn{\mathcal{X}}; each vector should be of length `nStrata-1`
+#' @return none
+#' @export
+make_parameters_demography_dynamic <- function(pars, b, d, m) {
+  stopifnot(is.environment(pars))
+  stopifnot(is.list(b), is.list(d), is.list(m))
+  stopifnot(all(vapply(b, function(x) {length(x) == pars$nStrata}, logical(1))))
+  stopifnot(all(vapply(d, function(x) {length(x) == pars$nStrata}, logical(1))))
+  stopifnot(all(vapply(m, function(x) {length(x) == pars$nStrata - 1}, logical(1))))
+  Hpar <- list()
+  class(Hpar) <- c('dynamic')
+  Hpar$b <- b
+  Hpar$d <- d
+  Hpar$m <- m
+  pars$Hpar <- Hpar
+}
