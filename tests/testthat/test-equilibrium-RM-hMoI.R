e13816e7d619c56f51b88f1c94636fd3c565532a
slwu89 <10673535+slwu89@users.noreply.github.com>
2022-12-05 11:41:52 -0800

    hMoI and RM eq test

--- tests/testthat/test-equilibrium-RM-hMoI.R
+++ tests/testthat/test-equilibrium-RM-hMoI.R
@@ -4,7 +4,7 @@ library(deSolve)
 
 numeric_tol <- 1e-5
 
-test_that("test equilibrium with RM adults (ODE), SIS humans, trace", {
+test_that("test equilibrium with RM adults (ODE), hMoI humans, trace", {
 
   # set number of patches and strata
   nPatches <- 2
@@ -14,7 +14,10 @@ test_that("test equilibrium with RM adults (ODE), SIS humans, trace", {
   # parameters
   b <- 0.55
   c <- 0.15
-  r <- 1/200
+  c1 <- 0.05
+  c2 <- 0.25
+  r1 <- 1/250
+  r2 <- 1/50
   wf <- rep(1, nStrata)
 
   f <- 0.3
@@ -39,10 +42,11 @@ test_that("test equilibrium with RM adults (ODE), SIS humans, trace", {
   OmegaEIP <- expm::expm(-Omega * tau)
   OmegaEIP_inv <- expm::expm(Omega * tau)
 
-  # human PfPR and H
-  pfpr <- rep(0.3, times = nStrata)
-  H <- rpois(n = nStrata, lambda = 1000)
-  X <- rbinom(n = nStrata, size = H, prob = pfpr)
+  # MoI at equilibrium
+  H <- c(100, 120)
+  m20 <- 1.5
+  h <- r2*m20
+  m10 <- h/r1
 
   # TaR
   Psi <- matrix(rexp(n = nStrata*nPatches), nStrata, nPatches)
@@ -50,7 +54,7 @@ test_that("test equilibrium with RM adults (ODE), SIS humans, trace", {
   Psi <- t(Psi)
 
   # derived EIR to sustain equilibrium pfpr
-  EIR <- diag(1/b, nStrata, nStrata) %*% ((r*X) / (H - X))
+  EIR <- rep(h/b, nStrata)
 
   # ambient pop
   W <- Psi %*% H
@@ -59,7 +63,11 @@ test_that("test equilibrium with RM adults (ODE), SIS humans, trace", {
   beta <- diag(wf) %*% t(Psi) %*% diag(1/as.vector(W), nPatches , nPatches)
 
   # kappa
-  kappa <- t(beta) %*% (X*c)
+  # kappa <- t(beta) %*% (X*c)
+  x1 <- pexp(q = rep(m10, nStrata))
+  x2 <- pexp(q = rep(m20, nStrata))
+  x <- ((c2 * x2) + (c1 * (x1 - x2))) * H
+  kappa <- t(beta) %*% x
 
   # equilibrium solutions for adults
   Z <- diag(1/(f*q), nPatches, nPatches) %*% ginv(beta) %*% EIR
@@ -85,7 +93,7 @@ test_that("test equilibrium with RM adults (ODE), SIS humans, trace", {
   params$calN <- calN
 
   make_parameters_MYZ_GeRM_ode(pars = params, g = g, sigma = sigma, calK = calK, tau = tau, f = f, q = q, nu = nu, eggsPerBatch = eggsPerBatch, M0 = as.vector(M), G0 = as.vector(G), Y0 = as.vector(Y), Z0 = as.vector(Z))
-  make_parameters_X_SIS(pars = params, Psi = Psi, b = b, c = c, r = r, X0 = X, H = H)
+  make_parameters_X_hMoI(pars = params, b = b, c1 = c1, c2 = c2, r1 = r1, r2 = r2, Psi = Psi, m10 = m10, m20 = m20, H = H)
   make_parameters_L_trace(pars = params, Lambda = as.vector(Lambda))
   make_parameters_exogenous_null(pars = params)
   make_parameters_vc_null(pars = params)
@@ -93,13 +101,14 @@ test_that("test equilibrium with RM adults (ODE), SIS humans, trace", {
   make_indices(params)
 
   # set initial conditions
-  y <- rep(NaN, max(params$X_ix))
+  y <- rep(NaN, params$max_ix)
   y[params$M_ix] <- as.vector(M)
   y[params$G_ix] <- as.vector(G)
   y[params$Y_ix] <- as.vector(Y)
   y[params$Z_ix] <- as.vector(Z)
   y[params$Upsilon_ix] <- as.vector(OmegaEIP)
-  y[params$X_ix] <- as.vector(X)
+  y[params$m1_ix] <- m10
+  y[params$m2_ix] <- m20
 
   # run simulation
   out <- deSolve::ode(y = y, times = c(0,50), func = xDE_diffeqn, parms = params, method = "lsoda")
@@ -108,10 +117,11 @@ test_that("test equilibrium with RM adults (ODE), SIS humans, trace", {
   expect_equal(as.vector(out[2, params$G_ix+1]), as.vector(G), tolerance = numeric_tol)
   expect_equal(as.vector(out[2, params$Y_ix+1]), as.vector(Y), tolerance = numeric_tol)
   expect_equal(as.vector(out[2, params$Z_ix+1]), as.vector(Z), tolerance = numeric_tol)
-  expect_equal(as.vector(out[2, params$X_ix+1]), as.vector(X), tolerance = numeric_tol)
+  expect_equal(as.vector(out[2L, params$m1_ix+1]), rep(m10, nStrata), tolerance = numeric_tol)
+  expect_equal(as.vector(out[2L, params$m2_ix+1]), rep(m20, nStrata), tolerance = numeric_tol)
 })
 
-test_that("test equilibrium with RM adults (DDE), SIS humans, trace", {
+test_that("test equilibrium with RM adults (DDE), hMoI humans, trace", {
 
   # set number of patches and strata
   nPatches <- 2
@@ -121,7 +131,10 @@ test_that("test equilibrium with RM adults (DDE), SIS humans, trace", {
   # parameters
   b <- 0.55
   c <- 0.15
-  r <- 1/200
+  c1 <- 0.05
+  c2 <- 0.25
+  r1 <- 1/250
+  r2 <- 1/50
   wf <- rep(1, nStrata)
 
   f <- 0.3
@@ -146,10 +159,11 @@ test_that("test equilibrium with RM adults (DDE), SIS humans, trace", {
   OmegaEIP <- expm::expm(-Omega * tau)
   OmegaEIP_inv <- expm::expm(Omega * tau)
 
-  # human PfPR and H
-  pfpr <- rep(0.3, times = nStrata)
-  H <- rpois(n = nStrata, lambda = 1000)
-  X <- rbinom(n = nStrata, size = H, prob = pfpr)
+  # MoI at equilibrium
+  H <- c(100, 120)
+  m20 <- 1.5
+  h <- r2*m20
+  m10 <- h/r1
 
   # TaR
   Psi <- matrix(rexp(n = nStrata*nPatches), nStrata, nPatches)
@@ -157,7 +171,7 @@ test_that("test equilibrium with RM adults (DDE), SIS humans, trace", {
   Psi <- t(Psi)
 
   # derived EIR to sustain equilibrium pfpr
-  EIR <- diag(1/b, nStrata, nStrata) %*% ((r*X) / (H - X))
+  EIR <- rep(h/b, nStrata)
 
   # ambient pop
   W <- Psi %*% H
@@ -166,7 +180,11 @@ test_that("test equilibrium with RM adults (DDE), SIS humans, trace", {
   beta <- diag(wf) %*% t(Psi) %*% diag(1/as.vector(W), nPatches , nPatches)
 
   # kappa
-  kappa <- t(beta) %*% (X*c)
+  # kappa <- t(beta) %*% (X*c)
+  x1 <- pexp(q = rep(m10, nStrata))
+  x2 <- pexp(q = rep(m20, nStrata))
+  x <- ((c2 * x2) + (c1 * (x1 - x2))) * H
+  kappa <- t(beta) %*% x
 
   # equilibrium solutions for adults
   Z <- diag(1/(f*q), nPatches, nPatches) %*% ginv(beta) %*% EIR
@@ -192,7 +210,7 @@ test_that("test equilibrium with RM adults (DDE), SIS humans, trace", {
   params$calN <- calN
 
   make_parameters_MYZ_GeRM_dde(pars = params, g = g, sigma = sigma, calK = calK, tau = tau, f = f, q = q, nu = nu, eggsPerBatch = eggsPerBatch, M0 = as.vector(M), G0 = as.vector(G), Y0 = as.vector(Y), Z0 = as.vector(Z))
-  make_parameters_X_SIS(pars = params, Psi = Psi, b = b, c = c, r = r, X0 = X, H = H)
+  make_parameters_X_hMoI(pars = params, b = b, c1 = c1, c2 = c2, r1 = r1, r2 = r2, Psi = Psi, m10 = m10, m20 = m20, H = H)
   make_parameters_L_trace(pars = params, Lambda = as.vector(Lambda))
   make_parameters_exogenous_null(pars = params)
   make_parameters_vc_null(pars = params)
@@ -200,13 +218,14 @@ test_that("test equilibrium with RM adults (DDE), SIS humans, trace", {
   make_indices(params)
 
   # set initial conditions
-  y <- rep(NaN, max(params$X_ix))
+  y <- rep(NaN, params$max_ix)
   y[params$M_ix] <- as.vector(M)
   y[params$G_ix] <- as.vector(G)
   y[params$Y_ix] <- as.vector(Y)
   y[params$Z_ix] <- as.vector(Z)
   y[params$Upsilon_ix] <- as.vector(OmegaEIP)
-  y[params$X_ix] <- as.vector(X)
+  y[params$m1_ix] <- m10
+  y[params$m2_ix] <- m20
 
   # run simulation
   out <- deSolve::dede(y = y, times = c(0,50), func = xDE_diffeqn, parms = params, method = "lsoda")
@@ -215,5 +234,6 @@ test_that("test equilibrium with RM adults (DDE), SIS humans, trace", {
   expect_equal(as.vector(out[2, params$G_ix+1]), as.vector(G), tolerance = numeric_tol)
   expect_equal(as.vector(out[2, params$Y_ix+1]), as.vector(Y), tolerance = numeric_tol)
   expect_equal(as.vector(out[2, params$Z_ix+1]), as.vector(Z), tolerance = numeric_tol)
-  expect_equal(as.vector(out[2, params$X_ix+1]), as.vector(X), tolerance = numeric_tol)
+  expect_equal(as.vector(out[2L, params$m1_ix+1]), rep(m10, nStrata), tolerance = numeric_tol)
+  expect_equal(as.vector(out[2L, params$m2_ix+1]), rep(m20, nStrata), tolerance = numeric_tol)
 })
