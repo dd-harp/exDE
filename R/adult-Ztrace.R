43fc6e87276e881ded9f20b01d7660486da95af9
David Smith <smitdave@gmail.com>
2023-03-21 13:59:46 -0700

    Ztrace related changes
    MosquitoBehavior.Ztrace added
    F_eggs.Ztrace added
    parameters f, q added
    class(xde) <- trace added
    transmission-trace added

--- R/adult-Ztrace.R
+++ R/adult-Ztrace.R
@@ -1,12 +1,24 @@
 # specialized methods for the adult mosquito Ztrace model
 
+#' @title Compute bloodfeeding and mortality rates
+#' @description Implements [MosquitoBehavior] for the Ztrace model.
+#' @inheritParams MosquitoBehavior
+#' @return a named [list]
+#' @export
+MosquitoBehavior.Ztrace <- function(t, y, pars) {
+  MosyBehavior <- list()
+  MosyBehavior$f <- rep(pars$MYZpar$f, 2)
+  MosyBehavior$q <- rep(pars$MYZpar$q, 2)
+  return(MosyBehavior)
+}
+
 #' @title Number of infective adults in each patch
 #' @description Implements [F_Z] for the Ztrace  model.
 #' @inheritParams F_Z
 #' @return a [numeric] vector of length `nHabitats`
 #' @export
 F_Z.Ztrace <- function(t, y, pars) {
-  with(pars$MYZpar, Zm*Zf(t, pars))
+  with(pars$MYZpar, return(Zm*Zf(t, pars)))
 }
 
 #' @title Number of infective adults in each patch
@@ -16,6 +28,15 @@ F_Z.Ztrace <- function(t, y, pars) {
 #' @export
 F_Z_lag.Ztrace <- function(t, y, pars, lag) {
   pars$MYZpar$Zf(t-lag)
+}
+
+#' @title Number of eggs laid by adult mosquitoes
+#' @description Implements [F_eggs] for the Ztrace model.
+#' @inheritParams F_eggs
+#' @return a [numeric] vector of length `nPatches`
+#' @export
+F_eggs.Ztrace <- function(t, y, pars) {
+  return(numeric(0))
 }
 
 #' @title Derivatives for aquatic stage mosquitoes
@@ -40,12 +61,20 @@ make_indices_MYZ.Ztrace <- function(pars) {
 #' @title Make parameters for Ztrace aquatic mosquito model
 #' @param pars an [environment]
 #' @param Zm a vector of mean mosquito densities
+#' @param f the blood feeding rate
+#' @param q the human fraction
 #' @param Zf a [function] of the form Zf(t, pars) that computes temporal fluctuations
 #' @return none
 #' @export
-make_parameters_MYZ_Ztrace <- function(pars, Zm=1, Zf="one") {
+make_parameters_MYZ_Ztrace <- function(pars, Zm, f, q, Zf="one") {
   stopifnot(is.numeric(Zm))
   MYZpar <- list()
+  xde <- "trace"
+  class(xde) <- "trace"
+  MYZpar$xde <- xde
+  MYZpar$Zm <- Zm
+  MYZpar$f <- f
+  MYZpar$q <- q
   class(MYZpar) <- 'Ztrace'
   if(Zf == "one") {
     MYZpar$Zf = function(t, pars){1}
