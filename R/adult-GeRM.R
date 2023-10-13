104dc3e98ea3cd1fb10c73f475cbc503188ca1b4
David Smith <smitdave@gmail.com>
2023-10-13 16:12:10 -0700

    Update adult-GeRM.R

--- R/adult-GeRM.R
+++ R/adult-GeRM.R
@@ -320,7 +320,7 @@ parse_deout_MYZ.GeRM <- function(varslist, deout, pars) {
   varslist$G = deout[,pars$MYZpar$G_ix+1]
   varslist$Y = deout[,pars$MYZpar$Y_ix+1]
   varslist$Z = deout[,pars$MYZpar$Z_ix+1]
-  varslist$g = with(varslist, g/M)
+  varslist$g = with(varslist, G/M)
   varslist$y = with(varslist, Y/M)
   varslist$z = with(varslist, Z/M)
   return(varslist)
